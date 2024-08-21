e8b4177507677c66d7a14090a04b7e92034c2e22
skavy359 <143738901+skavy359@users.noreply.github.com>
2024-08-21 18:46:42 +0530

    Create Scientific Format.cpp

---
+++ Scientific Format.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double a;
+    cin>>a;
+    cout<<scientific;
+    cout<<setprecision(5);
+    cout<<a;
+    return 0;
+}
