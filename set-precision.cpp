897b47e171d9e3128e6dfbdf755461fa16d16eca
skavy359 <143738901+skavy359@users.noreply.github.com>
2024-08-21 18:44:48 +0530

    Create set-precision.cpp

---
+++ set-precision.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    float a,b;
+    cin>>a;
+    cin>>b;
+    cout<<a/b<<endl;
+    cout<<std::setprecision(4);
+    cout<<a/b;
+    return 0;
+}
