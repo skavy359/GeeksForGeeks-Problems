d52ac910935b5b4f0d921e903852c6820fdecf2a
skavy359 <143738901+skavy359@users.noreply.github.com>
2024-08-27 22:33:19 +0530

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int gcd(int a,int b){
+    int len;
+    int res;
+
+    if(a>b){
+        len=a;
+    }
+    else{
+        len=b;
+    }
+
+    for(int i=1;i<len;i++){
+        if(a%i==0 && b%i==0){
+            res=i;
+        }
+    }
+    return res;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+    return 0;
+}
