f20a4ac9aaf26de43e178ce0ac0e3064d2c855e3
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-09-02 19:02:00 +0530

    Update Inverted Triangle.cpp

--- Inverted Triangle.cpp
+++ Inverted Triangle.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int space=i;
-        for(int j=0;j<2*n-i-1;j++){
-            if(space){
-                cout<<"  ";
-                space--;
-            }
-            else{
-                cout<<"* ";
-            }
+    int s;
+    cin>>s;
+    for(int i=1;i<=s;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"  ";
         }
+        for(int j=i;j<s;j++){
+            cout<<"* ";
+        }
+        for(int j=i;j<=s;j++){
+            cout<<"* ";
+        }
+
         cout<<endl;
     }
+
+    return 0;
 }
