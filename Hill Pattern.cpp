755df9806ab3ac0c7b7083aea9e97bb386edd6d5
Kavy Sharma <143738901+skavy359@users.noreply.github.com>
2024-09-02 18:58:10 +0530

    Create Hill Pattern.cpp

---
+++ Hill Pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int s;
+    cin>>s;
+    for(int i=1;i<=s;i++){
+        for(int j=i;j<=s;j++){
+            cout<<"  ";
+        }
+        for(int j=1;j<i;j++){
+            cout<<"* ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
